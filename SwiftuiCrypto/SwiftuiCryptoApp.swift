e77b7794747ca3d3c4665e1915b474a3d30e1bbe
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-06-29 17:19:48 -0300

    Initial Commit

---
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by userext on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
