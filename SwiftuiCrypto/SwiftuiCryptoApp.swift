b48a618612174c07795605d02484759adb6325a4
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-06-29 21:16:50 -0300

    2 video

--- SwiftuiCrypto/SwiftuiCryptoApp.swift
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct SwiftuiCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
+
